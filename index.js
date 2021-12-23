ffa68f3cf514db7e314750b50e2c0e750f29445c
Rory Tulk <rpstulk@gmail.com>
2021-12-23 11:30:57 -0500

    Updating to use new colours and models

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import axios from "axios";
-import { JSDOM } from "jsdom";
-import twilio from 'twilio';
+const axios = require ("axios");
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+const twilio = require('twilio');
 const config = {
     urls: [
             {
-                label:"NEURON_7_ORANGE",
-                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-7/2627.html?dwvar_2627_pv_rahmenfarbe=GY%2FOG"
+                label:"Neuron 6 Green",
+                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-6/3156.html?dwvar_3156_pv_rahmenfarbe=GN"
             },
             {
-                label: "NEURON_7_STEALTH",
-                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-7/2627.html?dwvar_2627_pv_rahmenfarbe=BK%2FGY"
+                label: "Neuron 6 Blue",
+                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-6/3156.html?dwvar_3156_pv_rahmenfarbe=BU"
             },
             {
-                label: "NEURON_6_BLUE",
-                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-6/2625.html?dwvar_2625_pv_rahmenfarbe=BU%2FBK"
+                label: "Neuron 5 Grey",
+                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-5/3155.html?dwvar_3155_pv_rahmenfarbe=GY%2FBK"
             },
             {   
-                label: "NEURON_6_STEALTH",
-                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-6/2625.html?dwvar_2625_pv_rahmenfarbe=BK%2FGY"
+                label: "Neuron 5 Red",
+                url: "https://www.canyon.com/en-ca/mountain-bikes/trail-bikes/neuron/al/neuron-5/3155.html?dwvar_3155_pv_rahmenfarbe=RD"
             }
         ],
     sizes: [
         "XS",
-        "S",
-        "XL"
+        "S"
     ]
     
 }
@@ -37,19 +37,20 @@ async function getBikePage(url) {
     try {
         const bikeResponse = await axios.get(url);
         console.log(`response status for ${url} - ${bikeResponse.status}`)
-        return bikeResponse.data;
+        //console.log(JSON.stringify(bikeResponse.data))
+        return await bikeResponse.data;
     } catch(e){
         console.error(e.message);
     }
 }
 
- async function parseBikeSizes(bikePage) {
+function parseBikeSizes(bikePage) {
 
-    const dom = await new JSDOM(bikePage);
-
+    const dom = new JSDOM(bikePage);
+   
     var availableNodes = dom.window.document.querySelectorAll('button.js-productConfigurationSelect');
     const availableSizes = [];
-
+    console.log(JSON.stringify(availableNodes))
     if (availableNodes) {
         availableNodes.forEach( node => availableSizes.push(node.attributes.getNamedItem('data-product-size').value))
     } 
@@ -60,14 +61,12 @@ async function getBikePage(url) {
 async function findBike(url) {
     console.log(`requesting bike info for ${url.label} at ${url.url}...`);
     const bikePage = await getBikePage(url.url);
-
-
     const availableSizes = await parseBikeSizes(bikePage);
 
     const desiredSizes = availableSizes.filter(size => config.sizes.includes(size));
 
     if (desiredSizes && desiredSizes.length > 0){
-        desiredSizes.forEach(size => 
+        desiredSizes.forEach(async size => 
             {
                 console.log(`${url.label} is available in size ${size}`);
                 notifyAvailableBike(url.label, size);
@@ -80,17 +79,15 @@ async function findBike(url) {
 async function notifyAvailableBike(bike, size){
     const client = twilio(accountSid, authToken);
 
-    client.messages
+    await client.messages
         .create({body:`${bike} is now available in size ${size}`, from:senderPhoneNumber, to:'6472142057'})
         .then(message => console.log(message.sid));
 }
 
-async function findAvailableBikes() {
-    config.urls.forEach(async url => findBike(url));
+function findAvailableBikes() {
+    config.urls.forEach(url => findBike(url));
 }
 
-
-
-findAvailableBikes();
-
-export default findAvailableBikes;
+exports.handler=async function(event, context) {
+    return findAvailableBikes();
+};
